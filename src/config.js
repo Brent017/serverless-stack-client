917e956d2257d5d14dff05af180166626e8ade22
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2020-04-09 16:41:42 -0600

    AWS amplify configured

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ee7l3521c7.execute-api.us-east-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_zcxC8GNVh",
+    APP_CLIENT_ID: "Y5sh7fr9uv09k2oahpvc3ct1jks",
+    IDENTITY_POOL_ID: "us-east-2:1a147fb2-e82b-4a55-8ac7-1208a7938eaa",
+  },
+};
