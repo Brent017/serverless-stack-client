700a8cc88d73ad2ece162d7d9cb1adb048d56d9b
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2020-04-15 11:59:16 -0600

    signup page added

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
   return (
@@ -10,6 +11,9 @@ export default function Routes() {
       <Route path="/" exact component={Home} />
       <Route exact path="/login">
         <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
       </Route>
       <Route>
         <NotFound />
